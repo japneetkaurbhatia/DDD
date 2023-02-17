9432b596b65c65f4b8902b25eea691e0353a6e04
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-02-17 11:34:58 +0530

    Add to cart

---
+++ src/main/java/Cart.java
@@ -0,0 +1,48 @@
+// add apple pencil to cart
+// add sony headphones
+// add 2 apple pencil to cart
+import java.util.*;
+
+class Product {
+    double price;
+    String name;
+    int quantity = 1;
+
+    public Product(String name, double price) {
+        this.price = price;
+        this.name = name;
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.price = price;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+}
+
+public class Cart {
+    public static ArrayList<String> Product = new ArrayList<String>();
+    public static  String product = "";
+    public static void main(String args[]) {
+        Product applePencil = new Product("Apple Pencil", 34.03);
+        product = addToCart(applePencil);
+        Product.add(product);
+
+        Product sonyHeadphone = new Product("Sony Headphone",340.03);
+        product = addToCart(sonyHeadphone);
+        Product.add(product);
+
+        Product applePencil2 = new Product("Apple Pencil", 34.03, 2);
+        product = addToCart(applePencil2);
+        Product.add(product);
+
+        System.out.println(Product);
+    }
+
+
+    public static String addToCart(Product product) {
+        String result = product.quantity + " " + product.name;
+        return result;
+    }
+}
